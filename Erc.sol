c2a79dba7acf49b6136a9b1ced600d141e3b6c00
Jeet Patel <jp2398d@gmail.com>
2022-04-27 16:12:44 +0530

    created ICOfunding contract

--- Erc.sol
+++ Erc.sol
@@ -7,23 +7,23 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @notice 2nd Sale Value at $0.02 (Seed Sale Quantity: 50 Million)
 /// @notice Final Sale for Remaining Tokens should be dynamically allocated.     
 /// @author Jeet Patel
-contract Erc is ERC20 {
-    uint256 public tokenAlloted;
-   
 
-    /**
-     * @title mints 100 million tokens and assigns it to the deployed address
-     */
+contract Erc is ERC20 {
+    // /**
+    //  * @title mints 100 million tokens and assigns it to the deployed address
+    //  */
     constructor() public ERC20('ERCTask', 'ERT') {
         // ownerAdd = msg.sender;
-        _mint(address(this), 100000000);        
+        _mint(address(this), 100000000);
     }
+}
 
-
-    /**
-     * @title transfer function
-     * @notice transfers token to the address that calls this function
-     */
+contract ICOfunding is Erc {    
+    uint256 public tokenAlloted;
+    // /**
+    //  * @title transfer function
+    //  * @notice transfers token to the address that calls this function
+    //  */
     function saleEvent() public payable {
         uint _tokenAmt = 0;
         if (tokenAlloted <= 30000000) {
